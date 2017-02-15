28ddffed4a9ea822223e4ca6ae3c8029270d37f6
James <jbalyo@appletreemediaworks.com>
2017-02-15 15:49:10 -0500

    Removed type:single requirement
    This might cause a conflict with the dropdown lists once that bug is
    fixed, but this test isn't working and I'm not certain why.

--- trunk/cf7-constantcontact.php
+++ trunk/cf7-constantcontact.php
@@ -695,7 +695,7 @@ class CTCTCF7 {
 	 */
 	public function save_form_settings( $args ) {
 		$cf_id = method_exists( $args, 'id' ) ? $args->id() : $args->id;
-		if ( $_POST['wpcf7-ctct']['accept'] == "" && strpos($_POST['wpcf7-ctct']['wpcf7-form'], " type:single ") !== false ) { 
+		if ( $_POST['wpcf7-ctct']['accept'] == "" ) { 
 			$_POST['wpcf7-ctct']['accept'] = "ctctcf7_lists"; 
 		}
 		update_option( 'cf7_ctct_' . $cf_id, $_POST['wpcf7-ctct'] );
